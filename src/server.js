94dd65efe9e974d0362bef0febad55ee6a23d48e
Arjun Junna <arjun9852@gmail.com>
2022-11-25 00:00:35 +0530

    feat : update notes controller and server file

--- src/server.js
+++ src/server.js
@@ -16,13 +16,19 @@ import {
   getAllNotesHandler,
   trashNoteHandler,
   updateNoteHandler,
-} from "./backend/controllers/NotesController";
+  notePinHandler,
+} from './backend/controllers/NotesController';
+import {
+  getAllTagsHandler,
+  getTagHandler,
+} from './backend/controllers/TagsController';
 import {
   deleteFromTrashHandler,
   getAllTrashNotesHandler,
   restoreFromTrashHandler,
 } from "./backend/controllers/TrashController";
 import { users } from "./backend/db/users";
+import { tags } from './backend/db/tags';
 
 export function makeServer({ environment = "development" } = {}) {
   const server = new Server({
@@ -34,52 +40,59 @@ export function makeServer({ environment = "development" } = {}) {
     models: {
       user: Model,
       notes: Model,
+      tags: Model,
     },
 
     seeds(server) {
       server.logging = false;
-      users.forEach((item) =>
-        server.create("user", {
+      users.forEach(item =>
+        server.create('user', {
           ...item,
           notes: [],
           archives: [],
           trash: [],
         })
       );
+      tags.forEach(item => server.create('tag', { ...item }));
     },
 
     routes() {
-      this.namespace = "api";
+      this.namespace = 'api';
 
       // auth routes (public)
-      this.post("/auth/signup", signupHandler.bind(this));
-      this.post("/auth/login", loginHandler.bind(this));
+      this.post('/auth/signup', signupHandler.bind(this));
+      this.post('/auth/login', loginHandler.bind(this));
+
+      // tags routes
+      this.get('/tags', getAllTagsHandler.bind(this));
+      this.get('/tags/:tagId', getTagHandler.bind(this));
 
       // user route (private)
-      this.get("/user", userProfilehandler.bind(this));
+      this.get('/user', userProfilehandler.bind(this));
 
       // notes routes (private)
-      this.get("/notes", getAllNotesHandler.bind(this));
-      this.post("/notes", createNoteHandler.bind(this));
-      this.post("/notes/:noteId", updateNoteHandler.bind(this));
-      this.delete("/notes/:noteId", deleteNoteHandler.bind(this));
-      this.post("/notes/archives/:noteId", archiveNoteHandler.bind(this));
-      this.post("/notes/trash/:noteId", trashNoteHandler.bind(this));
+      this.get('/notes', getAllNotesHandler.bind(this));
+      this.post('/notes', createNoteHandler.bind(this));
+      this.post('/notes/:noteId', updateNoteHandler.bind(this));
+      this.delete('/notes/:noteId', deleteNoteHandler.bind(this));
+      this.post('/notes/archives/:noteId', archiveNoteHandler.bind(this));
+      this.post('/notes/trash/:noteId', trashNoteHandler.bind(this));
+      this.post('/notes/pin/:noteId', notePinHandler.bind(this));
 
       // archive routes (private)
-      this.get("/archives", getAllArchivedNotesHandler.bind(this));
+      this.get('/archives', getAllArchivedNotesHandler.bind(this));
       this.post(
-        "/archives/restore/:noteId",
+        '/archives/restore/:noteId',
         restoreFromArchivesHandler.bind(this)
       );
       this.delete(
-        "/archives/delete/:noteId",
+        '/archives/delete/:noteId',
         deleteFromArchivesHandler.bind(this)
       );
       // trash routes (private)
-      this.get("/trash", getAllTrashNotesHandler.bind(this));
-      this.post("/trash/restore/:noteId", restoreFromTrashHandler.bind(this));
-      this.delete("/trash/delete/:noteId", deleteFromTrashHandler.bind(this));
+      this.get('/trash', getAllTrashNotesHandler.bind(this));
+      this.post('/trash/restore/:noteId', restoreFromTrashHandler.bind(this));
+      this.delete('/trash/delete/:noteId', deleteFromTrashHandler.bind(this));
     },
   });
   return server;
